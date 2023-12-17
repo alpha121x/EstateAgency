b0c9d44063258e8b0c987fb4c4d1eda23876e3f8
Abbasch <109143082+alpha121x@users.noreply.github.com>
2023-12-17 15:36:15 +0500

    Update intro-section.php

--- includes/page-parts/intro-section.php
+++ includes/page-parts/intro-section.php
@@ -1,27 +1,26 @@
 <div class="intro intro-carousel swiper position-relative">
 
-  <div class="swiper-wrapper">
+<div class="swiper-wrapper">
 
-    <div class="swiper-slide carousel-item-a intro-item bg-image" style="background-image: url(assets/img/slide-1.jpg)">
-      <div class="overlay overlay-a"></div>
-      <div class="intro-content display-table">
-        <div class="table-cell">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8">
-                <div class="intro-body">
-                  <p class="intro-title-top">Doral, Florida
-                    <br> 78345
-                  </p>
-                  <h1 class="intro-title mb-4 ">
-                    <span class="color-b">204 </span>
-                    <br>Mount Olive Road Two
-                  </h1>
-                  <p class="intro-subtitle intro-price">
-                    <a href="#"><span class="price-a">Buy | $ 12.000</span></a>
-                    <a href="#"><span class="price-a" data-bs-toggle="modal" data-bs-target="#exampleModal">Bid <i class="bi bi-coin"></i></span></a>
-                  </p>
-                </div>
+  <div class="swiper-slide carousel-item-a intro-item bg-image" style="background-image: url(assets/img/slide-1.jpg)">
+    <div class="overlay overlay-a"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="intro-body">
+                <p class="intro-title-top">Doral, Florida
+                  <br> 78345
+                </p>
+                <h1 class="intro-title mb-4 ">
+                  <span class="color-b">204 </span> Mount
+                  <br> Olive Road Two
+                </h1>
+                <p class="intro-subtitle intro-price">
+                  <a href="#"><span class="price-a">Buy | $ 12.000</span></a>
+                  <a href="#"><span class="price-a" data-bs-toggle="modal" data-bs-target="#exampleModal">Bid <i class="bi bi-coin"></i></span></a>
+                </p>
               </div>
             </div>
           </div>
@@ -29,7 +28,60 @@
       </div>
     </div>
   </div>
-  <div class="swiper-pagination"></div>
+  <div class="swiper-slide carousel-item-a intro-item bg-image" style="background-image: url(assets/img/slide-2.jpg)">
+    <div class="overlay overlay-a"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="intro-body">
+                <p class="intro-title-top">Doral, Florida
+                  <br> 78345
+                </p>
+                <h1 class="intro-title mb-4">
+                  <span class="color-b">204 </span> Rino
+                  <br> Venda Road Five
+                </h1>
+                <p class="intro-subtitle intro-price">
+                  <a href="#"><span class="price-a">Buy | $ 12.000</span></a>
+                  <a href="#"><span class="price-a">Bid <i class="bi bi-coin"></i></span></a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="swiper-slide carousel-item-a intro-item bg-image" style="background-image: url(assets/img/slide-3.jpg)">
+    <div class="overlay overlay-a"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8">
+              <div class="intro-body">
+                <p class="intro-title-top">Doral, Florida
+                  <br> 78345
+                </p>
+                <h1 class="intro-title mb-4">
+                  <span class="color-b">204 </span> Alira
+                  <br> Roan Road One
+                </h1>
+                <p class="intro-subtitle intro-price">
+                  <a href="#"><span class="price-a">Buy | $ 12.000</span></a>
+                  <a href="#"><span class="price-a">Bid <i class="bi bi-coin"></i></span></a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="swiper-pagination"></div>
 </div>
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -41,8 +93,8 @@
       </div>
       <div class="modal-body">
         <form action="" method="post">
-          <label for="bid-input" class="form-control">Add Bid Amount</label>
-          <input type="text" class="form-control" placeholder="Enter Bid Amoount Rs." name="bid" id="bid">
+        <label for="bid-input" class="form-control">Add Bid Amount</label>
+        <input type="text" class="form-control" placeholder="Enter Bid Amoount Rs." name="bid" id="bid">
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
