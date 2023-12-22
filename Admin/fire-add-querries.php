d5f57fcb7bc4d8cad550ea16caabc8ce078224de
Abbasch <109143082+alpha121x@users.noreply.github.com>
2023-12-22 19:13:17 +0500

    Update fire-add-querries.php

--- Admin/fire-add-querries.php
+++ Admin/fire-add-querries.php
@@ -33,20 +33,71 @@ if (isset($_POST['add-plot'])) {
     $plot_title = $_POST['plot_title'];
     $plot_location = $_POST['plot_location'];
     $plot_description = $_POST['plot_description'];
-    $plot_image = $_POST['plot_image'];
 
-    // Insert query using MeekroDB
-    $inserted = DB::insert('plot_listing', [
-        'plot_num' => $plot_num,
-        'plot_title' => $plot_title,
-        'plot_location' => $plot_location,
-        'plot_description' => $plot_description,
-        'plot_image' => $plot_image
-    ]);
+    // Check if the file is set and not empty
+    if (isset($_FILES["plot_image"]) && !empty($_FILES["plot_image"]["name"])) {
+        // File handling
+        $target_directory = "uploads/"; // Change this to your desired upload directory
+        $target_file = $target_directory . basename($_FILES["plot_image"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-    if ($inserted) {
-        header("Location: add_plot_listing.php");
+        // Check if the file is an actual image or a fake image
+        $check = getimagesize($_FILES["plot_image"]["tmp_name"]);
+        if ($check !== false) {
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        // Check file size
+        if ($_FILES["plot_image"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+            echo "Sorry, only JPG, JPEG, PNG, and GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        } else {
+            // If everything is ok, try to upload file
+            if (move_uploaded_file($_FILES["plot_image"]["tmp_name"], $target_file)) {
+                // Insert query using MeekroDB
+                $inserted = DB::insert('plot_listing', [
+                    'plot_num' => $plot_num,
+                    'plot_title' => $plot_title,
+                    'plot_location' => $plot_location,
+                    'plot_description' => $plot_description,
+                    'plot_image' => $target_file // Save the file path in the database
+                ]);
+
+                if ($inserted) {
+                    header("Location: add_plot_listing.php");
+                } else {
+                    echo "Error inserting data into the database.";
+                }
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+    } else {
+        echo "Please select a file to upload.";
     }
 }
 ?>
+
+
 
