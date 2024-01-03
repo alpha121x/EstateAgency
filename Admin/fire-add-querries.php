4e634528a9551173e44c51b93250f564e2b98c2f
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-01-04 00:10:29 +0500

    Update fire-add-querries.php

--- Admin/fire-add-querries.php
+++ Admin/fire-add-querries.php
@@ -35,38 +35,16 @@ if (isset($_POST['add-plot'])) {
     $plot_location = $_POST['plot_location'];
     $plot_description = $_POST['plot_description'];
     $plot_image = $_POST['plot_image'];
-
-    // handle file uploads
-
-    if(isset($_FILES['plot_image'])) {
-        $errors = array();
-        $file_name = $_FILES['plot_image']['name'];
-        $file_size = $_FILES['plot_image']['size'];
-        $file_tmp = $_FILES['plot_image']['tmp_name'];
-        $file_type = $_FILES['plot_image']['type'];
-        $file_ext = strtolower(end(explode('.', $_FILES['plot_image']['name'])));
-
-        $extensions = array("jpeg", "jpg", "png");
-
-        if(in_array($file_ext, $extensions) === false) {
-            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
-        }
+    
 
 
-        if(empty($errors) == true) {
-            move_uploaded_file($file_tmp, "uploads/" . $file_name);
-            echo "Success";
-        } else {
-            print_r($errors);
-        }
-
-        // Insert query using MeekroDB
+    // Insert query using MeekroDB
     $inserted = DB::insert('plot_listing', [
         'plot_num' => $plot_num,
         'plot_title' => $plot_title,
         'plot_location' => $plot_location,
         'plot_description' => $plot_description,
-        'plot_image' => $file_type // Save the file path in the database
+        'plot_image' => $plot_image // Save the file path in the database
     ]);
 
     if ($inserted) {
@@ -74,9 +52,5 @@ if (isset($_POST['add-plot'])) {
     } else {
         echo "Error inserting data into the database.";
     }
-    }
-
-
-    
 }
 ?>
