e7e94b74e3489de809bfe1efa8550c4d370308f7
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-01-29 00:48:41 +0500

    Update header-nav.php

--- Admin/include/header-nav.php
+++ Admin/include/header-nav.php
@@ -86,72 +86,58 @@ $user_data = DB::queryFirstRow("SELECT * FROM admin_users WHERE username=%s", $_
         </ul><!-- End Notification Dropdown Items -->
       </li>
       <!-- End Notification Nav -->
+      <?php
+      // Fetch messages from the contact_messages table
+      $messages = DB::query("SELECT * FROM contact_messages ORDER BY id DESC LIMIT 3");
 
-      <li class="nav-item dropdown">
+      // Check if there are messages
+      if ($messages) {
+      ?>
+        <li class="nav-item dropdown">
+          <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+            <i class="bi bi-chat-left-text"></i>
+            <span class="badge bg-success badge-number"><?php echo count($messages); ?></span>
+          </a><!-- End Messages Icon -->
 
-        <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-          <i class="bi bi-chat-left-text"></i>
-          <span class="badge bg-success badge-number">3</span>
-        </a><!-- End Messages Icon -->
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
+            <li class="dropdown-header">
+              You have <?php echo count($messages); ?> new messages
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
 
-        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-          <li class="dropdown-header">
-            You have 3 new messages
-            <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-          </li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
+            <?php foreach ($messages as $message) : ?>
+              <li class="message-item">
+                <a href="#">
+                  <img src="assets/img/messages-1.jpg" alt="" class="rounded-circle">
+                  <div>
+                    <h4><?php echo $message['name']; ?></h4>
+                    <p><?php echo substr($message['message'], 0, 50); ?>...</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+            <?php endforeach; ?>
 
-          <li class="message-item">
-            <a href="#">
-              <img src="assets/img/messages-1.jpg" alt="" class="rounded-circle">
-              <div>
-                <h4>Maria Hudson</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>4 hrs. ago</p>
-              </div>
+            <li class="dropdown-footer">
+              <a href="#">Show all messages</a>
+            </li>
+          </ul><!-- End Messages Dropdown Items -->
+        </li>
+      <?php
+      } else {
+        // If there are no messages
+        echo '<li class="nav-item dropdown">
+            <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                <i class="bi bi-chat-left-text"></i>
             </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-
-          <li class="message-item">
-            <a href="#">
-              <img src="assets/img/messages-2.jpg" alt="" class="rounded-circle">
-              <div>
-                <h4>Anna Nelson</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>6 hrs. ago</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-
-          <li class="message-item">
-            <a href="#">
-              <img src="assets/img/messages-3.jpg" alt="" class="rounded-circle">
-              <div>
-                <h4>David Muldon</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>8 hrs. ago</p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-
-          <li class="dropdown-footer">
-            <a href="#">Show all messages</a>
-          </li>
-
-        </ul><!-- End Messages Dropdown Items -->
-
-      </li><!-- End Messages Nav -->
+          </li>';
+      }
+      ?>
+      <!-- End Messages Nav -->
 
       <li class="nav-item dropdown pe-3">
 
