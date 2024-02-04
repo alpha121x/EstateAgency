0615a7ee9f650cc3076dbb792e4c1f2a95ecf9e3
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-04 22:07:00 +0500

    added cookie setting

--- visit_count.php
+++ visit_count.php
@@ -1,16 +1,22 @@
-<!-- <?php
-// $cookieName = 'visit_count';
+ <?php
+    $cookieName = 'visit_count';
 
-// // Check if the cookie is set
-// if (isset($_COOKIE[$cookieName])) {
-//     $visitCount = $_COOKIE[$cookieName];
-// } else {
-//     $visitCount = 1;
-// }
+    // Check if the cookie is set
+    if (isset($_COOKIE[$cookieName])) {
+        // Cookie is set, no need to increment the count
+        $visitCount = $_COOKIE[$cookieName];
+    } else {
+        // Cookie is not set, or it has expired
+        $visitCount = 1;
 
-// // Set the updated count in the cookie
-// setcookie($cookieName, $visitCount, time() + 3600 * 24); // Cookie expires in 24 hours
+        // Set the updated count in the cookie with an expiration time at the end of the day
+        setcookie($cookieName, $visitCount, strtotime('tomorrow') - time());
 
+        // Insert the visit count along with the current date and time into the visited_count table
+        $currentDateTime = date('Y-m-d H:i:s');
+        DB::insert('visited_count', ['visit_count' => $visitCount, 'visit_datetime' => $currentDateTime]);
+    }
 
-?>
- -->
+    // Now $visitCount contains the updated count
+    ?>
+
