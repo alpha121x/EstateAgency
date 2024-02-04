f0810ac65c8bda7dbf871caa9492b67241b310d5
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-04 17:25:28 +0500

    changes in side-nav

--- Admin/include/side-nav.php
+++ Admin/include/side-nav.php
@@ -9,58 +9,79 @@
          <span>Dashboard</span>
        </a>
      </li><!-- End Dashboard Nav --> 
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="add-user-profile">
-           <i class="bi bi-person-fill-add"></i>
-             <span>Add Users</span>
-           </a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="users_profile">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>User Profile</span>
-           </a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="admin_users">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Users</span>
-           </a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="add-posts">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Add Posts</span>
-           </a>
-         </li>
-         <li class="nav-item">
-         <a class="nav-link collapsed"  href="posts">
-         <i class="bi bi-person-fill-lock"></i>
-           <span>Posts</span>
-         </a>
-       </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="plot_listing">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Plot Listing</span>
-           </a>
-         </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="add_plot_listing">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Add Plots Listing</span>
-           </a>
-         </li>
-         <li class="nav-item">
-         <a class="nav-link collapsed"  href="agents">
-         <i class="bi bi-person-fill-lock"></i>
-           <span>Agents</span>
-         </a>
-       </li>
-         <li class="nav-item">
-           <a class="nav-link collapsed"  href="add-agents">
-           <i class="bi bi-person-fill-lock"></i>
-             <span>Add Agents</span>
-           </a>
-         </li>
+     <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-people-fill"></i><span>Users</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="components-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="add-user-profile">
+            <i class="bi bi-circle-fill text-primary"></i></i><span>Add Users</span>
+            </a>
+          </li>
+          <li>
+            <a href="users_profile">
+              <i class="bi bi-circle-fill text-primary"></i><span>User Profile</span>
+            </a>
+          </li>
+          <li>
+            <a href="admin_users">
+              <i class="bi bi-circle-fill text-primary"></i><span>Users</span>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#posts-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-file-post-fill"></i><span>Posts</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="posts-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="add-posts">
+            <i class="bi bi-circle-fill text-primary"></i></i><span>Add Posts</span>
+            </a>
+          </li>
+          <li>
+            <a href="posts">
+              <i class="bi bi-circle-fill text-primary"></i><span>Posts</span>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#plots-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-houses-fill"></i><span>Plots</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="plots-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="add_plot_listing">
+            <i class="bi bi-circle-fill text-primary"></i></i><span>Add Plots</span>
+            </a>
+          </li>
+          <li>
+            <a  href="plot_listing">
+              <i class="bi bi-circle-fill text-primary"></i><span>Plots Listing</span>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#agents-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-person-vcard-fill"></i><span>Agents</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="agents-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="agents">
+            <i class="bi bi-circle-fill text-primary"></i></i><span>Agents</span>
+            </a>
+          </li>
+          <li>
+            <a  href="add-agents">
+              <i class="bi bi-circle-fill text-primary"></i><span>Add Agents</span>
+            </a>
+          </li>
+        </ul>
+      </li>
    </aside><!-- End Sidebar-->
