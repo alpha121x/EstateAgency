0cf4ca37e15c0637ad0600ea05852a511887f96c
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-04 17:47:20 +0500

    Update main.php

--- Admin/include/main.php
+++ Admin/include/main.php
@@ -17,9 +17,8 @@
       <div class="col-lg-8">
         <div class="row">
 
-          <!-- Sales Card -->
           <div class="col-xxl-4 col-md-6">
-            <div class="card info-card sales-card">
+            <div class="card info-card viewers-card">
 
               <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
@@ -35,25 +34,24 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Sales <span>| Today</span></h5>
+                <h5 class="card-title">Website Viewers <span>| Today</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-cart"></i>
+                    <i class="bi bi-eye"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>145</h6>
-                    <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                    <h6>2650</h6>
+                    <span class="text-success small pt-1 fw-bold">15%</span> <span class="text-muted small pt-2 ps-1">increase</span>
 
                   </div>
                 </div>
               </div>
             </div>
+          </div><!-- End Website Viewers Card -->
 
-          </div><!-- End Sales Card -->
-          <!-- Revenue Card -->
           <div class="col-xxl-4 col-md-6">
-            <div class="card info-card revenue-card">
+            <div class="card info-card bids-card">
 
               <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
@@ -69,27 +67,25 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Revenue <span>| This Month</span></h5>
+                <h5 class="card-title">Total Bids <span>| This Month</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-currency-dollar"></i>
+                    <i class="bi bi-gift"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>$3,264</h6>
-                    <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                    <h6>78</h6>
+                    <span class="text-success small pt-1 fw-bold">10%</span> <span class="text-muted small pt-2 ps-1">increase</span>
 
                   </div>
                 </div>
               </div>
 
             </div>
-          </div><!-- End Revenue Card -->
+          </div><!-- End Total Bids Card -->
 
-          <!-- Customers Card -->
           <div class="col-xxl-4 col-xl-12">
-
-            <div class="card info-card customers-card">
+            <div class="card info-card amounts-card">
 
               <div class="filter">
                 <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
@@ -105,23 +101,23 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Customers <span>| This Year</span></h5>
+                <h5 class="card-title">Total Bids Amounts <span>| This Year</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-people"></i>
+                    <i class="bi bi-cash"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>1244</h6>
-                    <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
+                    <h6>$12,450</h6>
+                    <span class="text-danger small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
 
                   </div>
                 </div>
 
               </div>
             </div>
+          </div><!-- End Total Bids Amounts Card -->
 
-          </div><!-- End Customers Card -->
 
         </div>
 
