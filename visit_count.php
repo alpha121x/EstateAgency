51c723c96093cf7acf9d93bf6c5e60762a8b3886
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-04 19:37:37 +0500

    Update visit_count.php

--- visit_count.php
+++ visit_count.php
@@ -1,16 +1,16 @@
-<?php
-$cookieName = 'visit_count';
+<!-- <?php
+// $cookieName = 'visit_count';
 
-// Check if the cookie is set
-if (isset($_COOKIE[$cookieName])) {
-    $visitCount = $_COOKIE[$cookieName];
-} else {
-    $visitCount = 1;
-}
+// // Check if the cookie is set
+// if (isset($_COOKIE[$cookieName])) {
+//     $visitCount = $_COOKIE[$cookieName];
+// } else {
+//     $visitCount = 1;
+// }
 
-// Set the updated count in the cookie
-setcookie($cookieName, $visitCount, time() + 3600 * 24); // Cookie expires in 24 hours
+// // Set the updated count in the cookie
+// setcookie($cookieName, $visitCount, time() + 3600 * 24); // Cookie expires in 24 hours
 
 
 ?>
-
+ -->
