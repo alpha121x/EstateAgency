4cfc86a6ca6a10794793f19fe341b3b8416dce34
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-16 19:18:41 +0500

    changes

---
+++ Admin/bid-report.php
@@ -0,0 +1,59 @@
+<!--  write code for shwoing posts in a data table -->
+<?php
+include('db_config.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Posts</title>
+  <?php include("include/linked-files.php") ?>
+</head>
+
+<body>
+
+    <?php include("include/header-nav.php") ?>
+
+    <?php include("include/side-nav.php") ?>
+
+
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+            <h1>Posts</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item active">Reports</li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
+
+        <section class="section">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Reports</h5>
+                            <p>Bids Report</p>
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    </main><!-- End #main -->
+
+
+    <?php include("include/footer.php") ?>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <?php include("include/script-files.php") ?>
+
+</body>
+
+</html>
--- Admin/include/side-nav.php
+++ Admin/include/side-nav.php
@@ -74,12 +74,12 @@
         </ul>
       </li>
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#plots-nav" data-bs-toggle="collapse" href="#">
+        <a class="nav-link collapsed" data-bs-target="#reports-nav" data-bs-toggle="collapse" href="#">
         <i class="bi bi-houses-fill"></i><span>Reports</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
-        <ul id="plots-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+        <ul id="reports-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
           <li>
-            <a href="bid-report">
+            <a href="reports/bid-report">
             <i class="bi bi-circle-fill text-primary"></i></i><span>Bid Report</span>
             </a>
           </li>
