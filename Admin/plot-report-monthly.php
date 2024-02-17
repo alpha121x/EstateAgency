fd7870eade0ce67672cf651bfddc2471c53d923b
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-18 01:58:52 +0500

    updates

--- Admin/plot-report-monthly.php
+++ Admin/plot-report-monthly.php
@@ -100,7 +100,7 @@ include('db_config.php'); ?>
                             <!-- End Table with stripped rows -->
 
                             <div class="table-responsive">
-                                <table class="table table-bordered" style="background-color: white;">
+                                <table id="individualPlotsTable" class="table table-bordered" style="background-color: white;">
                                     <thead>
                                         <h5 class="card-title">Individual Plots Monthly Report</h5>
                                         <tr>
@@ -184,6 +184,12 @@ include('db_config.php'); ?>
 
 
 
+                            <script>
+                                $(document).ready(function() {
+                                    // Initialize DataTable
+                                    $('#individualPlotsTable').DataTable();
+                                });
+                            </script>
 
 
 
