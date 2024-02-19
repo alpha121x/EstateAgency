8f42b23cbe0f8c59bcb73f3369793946676c45ee
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-19 18:49:26 +0500

    Update home-content-listing.php

--- Admin/home-content-listing.php
+++ Admin/home-content-listing.php
@@ -24,7 +24,7 @@
       <h1>Plot Lisiting</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
           <li class="breadcrumb-item">Content Slider Listing</li>
         </ol>
       </nav>
