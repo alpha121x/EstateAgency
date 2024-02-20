5d150d6b66a9a47cc25b13d9c9945e85cf6e594d
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-20 22:32:53 +0500

    Update home-content-listing.php

--- Admin/home-content-listing.php
+++ Admin/home-content-listing.php
@@ -21,7 +21,7 @@
   <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Plot Lisiting</h1>
+      <h1>Property Listing</h1>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.php">Home</a></li>
@@ -36,7 +36,7 @@
 
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Content Slider Lisitng</h5>
+              <h5 class="card-title">Content Slider Listing</h5>
               <p>View All</p>
 
               <!-- Table with stripped rows -->
@@ -44,9 +44,9 @@
                 <thead>
                   <tr>
                     <th scope="col">#</th>
-                    <th scope="col">Plot number</th>
-                    <th scope="col">Plot Title</th>
-                    <th scope="col">Plot Location</th>
+                    <th scope="col">Property number</th>
+                    <th scope="col">Property Title</th>
+                    <th scope="col">Property Location</th>
                     <th scope="col">Date</th>
                     <th scope="col" class="text-center">Changes</th>
                   </tr>
@@ -57,26 +57,25 @@
                   require_once("include/classes/meekrodb.2.3.class.php");
 
                   // Select all users from the admin_users table
-                  $plots = DB::query("SELECT * FROM home_content_slider ORDER BY added_on DESC");
+                  $properties = DB::query("SELECT * FROM home_content_slider ORDER BY added_on DESC");
 
-                  if ($plots) {
+                  if ($properties) {
                     $index = 1;
-                    foreach ($plots as $plot) {
+                    foreach ($properties as $property) {
                       // Assign variables from the fetched row
-                      $id = $plot['id'];
-                      $plot_num = $plot['plot_num'];
-                      $plot_title = $plot['plot_title'];
-                      $plot_location = $plot['plot_location'];
-                      $plot_description = $plot['plot_description'];
-                      $added_on = $plot['added_on'];
-                      $plot_image = $plot['plot_image'];
+                      $id = $property['id'];
+                      $property_num = $property['property_num'];
+                      $property_title = $property['property_title'];
+                      $property_location = $property['property_location'];
+                      $added_on = $property['added_on'];
+                      $property_image = $property['property_image'];
                   ?>
                       <!-- Display data in the rows -->
                       <tr>
                         <td><?php echo $index; ?></td>
-                        <td><?php echo $plot_num; ?></td>
-                        <td><?php echo $plot_title; ?></td>
-                        <td><?php echo $plot_location ?></td>
+                        <td><?php echo $property_num; ?></td>
+                        <td><?php echo $property_title; ?></td>
+                        <td><?php echo $property_location ?></td>
                         <td><?php echo $added_on; ?></td>
                         <td class="text-center">
                           <a href='edit-content-slider?id=<?php echo $id; ?>' class="btn btn-success btn-sm"><i class="bi bi-pencil-square"></i></a>
@@ -84,7 +83,7 @@
                           if ($_SESSION['user_type'] == 'admin') {
                           ?>
                             |
-                            <a href='#' class="btn btn-danger btn-sm"><i class="bi bi-trash"></i></a>
+                            <a href='delete-content-slider.php?id=<?php echo $id; ?>' class="btn btn-danger btn-sm"><i class="bi bi-trash"></i></a>
                           <?php
                           }
                           ?>
