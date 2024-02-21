8924a6f7602a483a7e2efd55a79f6553d70e190f
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-21 20:03:05 +0500

    Update intro-section.php

--- includes/page-parts/intro-section.php
+++ includes/page-parts/intro-section.php
@@ -37,7 +37,6 @@
                       <p class="intro-subtitle intro-price">
                         <?php if ($property_status == 1) : ?>
                           <a href="#"><span class="price-a">Buy | Rs. <?php echo $property_price; ?></span></a>
-                          <span type="button" class="price-a" data-bs-toggle="modal" data-bs-target="#Modal<?php echo $slide['id']; ?>" data-property-id="<?php echo $slide['id']; ?>">Bid</span>
                         <?php elseif ($property_status == 3) : ?>
                           <span class="price-a">Sold</span>
                         <?php endif; ?>
@@ -49,72 +48,6 @@
             </div>
           </div>
         </div>
-
-        <!-- Modal -->
-<?php foreach ($properties as $property) { ?>
-  <div class="modal fade" id="Modal<?php echo $property['plot_id']; ?>" tabindex="-1" aria-labelledby="ModalLabel<?php echo $property['plot_id']; ?>" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <?php
-          // echo $property['plot_id'];
-          // Assuming $property['added_on'] contains the added-on date from your database
-          $addedOnDate = date('Y-m-d', strtotime($property['added_on']));
-          date_default_timezone_set('Asia/Karachi');
-          $currentDate = date("Y-m-d");
-
-          // Calculate the difference in days
-          $daysDifference = floor(strtotime($currentDate) - strtotime($addedOnDate)) / (60 * 60 * 24);
-
-          // Calculate the remaining bidding days
-          $daysLeft = max(0, $property['bidding_days'] - $daysDifference);
-
-          // If there are still bidding days left, display the time left
-          if ($daysLeft > 0) {
-            echo '<h1 class="modal-title fs-5" id="ModalLabel' . $property['plot_id'] . '">&nbsp;Bidding Time Left: ' . $daysLeft . ' days</h1>';
-          } else {
-            echo '<h1 class="modal-title fs-5" id="ModalLabel' . $property['plot_id'] . '">&nbsp;Bidding has ended</h1>';
-          }
-          ?>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <form action="bid" method="post">
-            <label for="name" class="form-control fw-bold">Username<input type="hidden" name="plot_id" value="<?php echo $property['plot_id']; ?>" id="propertyIdInput"></label>
-            <input type="text" required class="form-control" placeholder="Enter your username" name="username" id="username">
-            <br>
-            <label for="email" class="form-control fw-bold">Email</label>
-            <input type="email" required class="form-control" placeholder="Enter your email" name="email" id="email">
-            <br>
-            <label for="bid" class="form-control fw-bold">Bid Amount</label>
-            <?php
-            // Fetch minimum bid amount and plot price from the database
-            $minBidAmount = $property['plot_price'];
-
-            // Display the minimum bid amount and plot price in the input field
-            echo '<input type="text" required class="form-control" placeholder="Minimum Bid: Rs. ' . $minBidAmount . '" name="bid" id="bid">';
-            ?>
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-          <button type="submit" name="add-bid" class="btn btn-success">Submit</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-<?php } ?>
-
-<script>
-  // JavaScript to update the hidden input value when Bid button is clicked
-  document.querySelectorAll('.price-a').forEach(function(bidButton) {
-    bidButton.addEventListener('click', function() {
-      var propertyId = this.getAttribute('data-property-id');
-      document.getElementById('propertyIdInput').value = propertyId;
-    });
-  });
-</script>
 
     <?php
       }
