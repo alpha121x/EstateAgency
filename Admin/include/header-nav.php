341e654b458a8c1c3dee15417d3beb44dbd06e84
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-22 22:18:38 +0500

    Update header-nav.php

--- Admin/include/header-nav.php
+++ Admin/include/header-nav.php
@@ -70,8 +70,11 @@ $user_data = DB::queryFirstRow("SELECT * FROM admin_users WHERE username=%s", $_
           return "$years years ago";
         }
       }
+
+      $user = $_SESSION['user'];
       $notificationCount = DB::queryFirstField("SELECT COUNT(*) FROM notifications WHERE is_read = 0");
-      $notifications = DB::query("SELECT * FROM notifications WHERE is_read = 0 ORDER BY bid_date DESC");
+      $notifications = DB::query("SELECT * FROM notifications WHERE is_read = 0 ORDER BY bid_date DESC 
+      AND created_by=$user");
       $counter = 0;
       ?>
 
