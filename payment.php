873f72f7a1d549e87b776ce50cf2ce2660f9d953
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-22 20:48:51 +0500

    Update payment.php

--- payment.php
+++ payment.php
@@ -28,57 +28,69 @@
         <div class="row d-flex justify-content-center">
           <div class="col-md-8 col-lg-6 col-xl-4">
             <div class="card rounded-3">
-              <div class="card-body mx-1 my-2">
-                <div class="d-flex align-items-center">
-                  <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="black" class="bi bi-credit-card" viewBox="0 0 16 16">
-                      <path d="M15 2H1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zm0 1v2H1V4h14zM0 7h16v2H0V7zm0 4h16v2H0v-2z" />
-                      <text x="50%" y="75%" font-size="3" font-family="Arial" fill="white" text-anchor="middle">Visa</text>
-                    </svg>
+              <form action="#">
+                <div class="card-body mx-1 my-2">
+                  <div class="d-flex align-items-center">
+                    <div>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="black" class="bi bi-credit-card" viewBox="0 0 16 16">
+                        <path d="M15 2H1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zm0 1v2H1V4h14zM0 7h16v2H0V7zm0 4h16v2H0v-2z" />
+                        <text x="50%" y="75%" font-size="3" font-family="Arial" fill="white" text-anchor="middle">Visa</text>
+                      </svg>
 
-                    &nbsp;
-                  </div>
-                  <div>
-                    <p class="d-flex flex-column mb-0">
-                      <b>Martina Thomas</b><span class="small text-muted">**** 8880</span>
-                    </p>
-                  </div>
-                </div>
-
-                <div class="pt-3">
-                  <div class="d-flex flex-row pb-3">
-                    <div class="rounded border border-primary border-2 d-flex w-100 p-3 align-items-center" style="background-color: rgba(18, 101, 241, 0.07);">
-                      <div class="d-flex align-items-center pe-3">
-                        <input class="form-check-input" type="radio" name="radioNoLabelX" id="radioNoLabel11" value="" aria-label="..." checked />
-                      </div>
-                      <div class="d-flex flex-column">
-                        <p class="mb-1 small text-primary">Total amount due</p>
-                        <h6 class="mb-0 text-primary">$8245</h6>
-                      </div>
+                      &nbsp;
+                    </div>
+                    <div>
+                      <p class="d-flex flex-column mb-0">
+                        <b>Martina Thomas</b><span class="small text-muted">**** 8880</span>
+                      </p>
                     </div>
                   </div>
 
-                  <div class="d-flex flex-row pb-3">
-                    <div class="rounded border d-flex w-100 px-3 py-2 align-items-center">
-                      <div class="d-flex align-items-center pe-3">
-                        <input class="form-check-input" type="radio" name="radioNoLabelX" id="radioNoLabel22" value="" aria-label="..." />
+                  <div class="pt-3">
+                    <div class="mb-3">
+                      <label for="cardNumber" class="form-label">Card Number</label>
+                      <input type="text" class="form-control" id="cardNumber" placeholder="**** **** **** 8880">
+                    </div>
+                    <div class="mb-3">
+                      <label for="cardholderName" class="form-label">Cardholder Name</label>
+                      <input type="text" class="form-control" id="cardholderName" placeholder="Martina Thomas">
+                    </div>
+
+                    <div class="d-flex flex-row pb-3">
+                      <div class="rounded border border-primary border-2 d-flex w-100 p-3 align-items-center" style="background-color: rgba(18, 101, 241, 0.07);">
+                        <div class="d-flex align-items-center pe-3">
+                          <input class="form-check-input" type="radio" name="radioNoLabelX" id="radioNoLabel11" value="" aria-label="..." checked />
+                        </div>
+                        <div class="d-flex flex-column">
+                          <p class="mb-1 small text-primary">Total amount due</p>
+                          <h6 class="mb-0 text-primary">$8245</h6>
+                        </div>
                       </div>
-                      <div class="d-flex flex-column py-1">
-                        <p class="mb-1 small text-primary">Other amount</p>
-                        <div class="d-flex flex-row align-items-center">
-                          <h6 class="mb-0 text-primary pe-1">$</h6>
-                          <input type="text" class="form-control form-control-sm" id="numberExample" style="width: 55px;" />
+                    </div>
+
+                    <div class="d-flex flex-row pb-3">
+                      <div class="rounded border d-flex w-100 px-3 py-2 align-items-center">
+                        <div class="d-flex align-items-center pe-3">
+                          <input class="form-check-input" type="radio" name="radioNoLabelX" id="radioNoLabel22" value="" aria-label="..." />
+                        </div>
+                        <div class="d-flex flex-column py-1">
+                          <p class="mb-1 small text-primary">Other amount</p>
+                          <div class="d-flex flex-row align-items-center">
+                            <h6 class="mb-0 text-primary pe-1">$</h6>
+                            <input type="text" class="form-control form-control-sm" id="numberExample" style="width: 55px;" />
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
 
-                <div class="d-flex justify-content-between align-items-center pb-1">
-                  <a href="#!" class="text-muted">Go back</a>
-                  <button type="button" class="btn btn-primary btn-lg">Pay amount</button>
+                  <div class="d-flex justify-content-between align-items-center pb-1">
+                    <a href="#!" class="text-muted">Go back</a>
+                    <button type="submit" class="btn btn-primary btn-lg">Pay amount</button>
+                  </div>
                 </div>
-              </div>
+              </form>
+
             </div>
           </div>
         </div>
