2ddab0c39d0f3b826016b211f83da6977fd5ca0b
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-02-28 23:49:13 +0500

    Update fire-add-querries.php

--- Admin/fire-add-querries.php
+++ Admin/fire-add-querries.php
@@ -272,12 +272,14 @@ if (isset($_POST["add-testimonial"])) {
      // File Upload
      $uploadsFolder = 'uploads/';
      $agent_image = $uploadsFolder . basename($_FILES['testinomial_image']['name']);
-     $uploadSuccess = move_uploaded_file($_FILES['agent_image']['tmp_name'], $agent_image);
+     $uploadSuccess = move_uploaded_file($_FILES['testinomial_image']['tmp_name'], $agent_image);
  
      if (!$uploadSuccess) {
          echo "Error uploading file.";
          exit;
      }
+
+     
 
    
 }
